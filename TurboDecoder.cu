a9eff1afc8217aa9a725e8ab4da8d52e26e37f37
liuhatry <liuhatry@ittc626-desktop>
2013-04-10 18:01:00 +0800

    lte interleave temp

--- TurboDecoder.cu
+++ TurboDecoder.cu
@@ -26,10 +26,53 @@ using namespace std;
 #define INIFINITY  1E+10
 
 #define MIN 1E-300
-#define L_TOTAL 1024// if u want to use block interleave,L_TOTAL must = x^2
+#define L_TOTAL 6144 // if u want to use block interleave,L_TOTAL must = x^2
 #define MAXITER 10
 #define	FRAME_NUM 10
 #define AlphaBetaTHREAD_NUM 4
+
+#define THREAD_NUM 768
+#define BLOCK_NUM 16
+
+typedef unsigned char BYTE;
+typedef int INT;
+typedef unsigned int UINT;
+typedef int BOOL;
+
+UINT m_Inter_table[L_TOTAL];
+UINT LTE_Inter_table[L_TOTAL][3] =
+	{40,3,10,	48,7,12,    56,19,42,	64,7,16,    72,7,18,    80,11,20,   
+    88,5,22,    96,11,24,   104,7,26,   112,41,84,  120,103,90, 128,15,32,  
+    136,9,34,   144,17,108, 152,9,38,	160,21,120, 168,101,84, 176,21,44,  
+    184,57,46,  192,23,48,  200,13,50,  208,27,52,  216,11,36,  224,27,56,  
+    232,85,58,  240,29,60,  248,33,62,  256,15,32,  264,17,198, 272,33,68,  
+    280,103,210,288,19,36,  296,19,74,  304,37,76,  312,19,78,  320,21,120, 
+    328,21,82,  336,115,84, 344,193,86, 352,21,44,  360,133,90, 368,81,46,  
+    376,45,94,  384,23,48,  392,243,98, 400,151,40, 408,155,102,416,25,52,  
+    424,51,106, 432,47,72,  440,91,110, 448,29,168, 456,29,114, 464,247,58, 
+    472,29,118, 480,89,180, 488,91,122, 496,157,62, 504,55,84,  512,31,64,  
+    528,17,66,  544,35,68,  560,227,420,576,65,96,  592,19,74,  608,37,76,  
+    624,41,234, 640,39,80,  656,185,82, 672,43,252, 688,21,86,  704,155,44, 
+    720,79,120, 736,139,92, 752,23,94,  768,217,48, 784,25,98,  800,17,80,  
+    816,127,102,832,25,52,  848,239,106,864,17,48,  880,137,110,896,215,112,
+    912,29,114, 928,15,58,  944,147,118,960,29,60,  976,59,122, 992,65,124, 
+    1008,55,84, 1024,31,64, 1056,17,66, 1088,171,204,1120,67,140,1152,35,72,
+    1184,19,74, 1216,39,76, 1248,19,78, 1280,199,240,1312,21,82,1344,211,252,
+    1376,21,86, 1408,43,88, 1440,149,60,1472,45,92, 1504,49,846,1536,71,48, 
+    1568,13,28, 1600,17,80, 1632,25,102,1664,183,104,1696,55,954,1728,127,96,
+    1760,27,110,1792,29,112,1824,29,114,1856,57,116,1888,45,354,1920,31,120,
+    1952,59,610,1984,185,124,2016,113,420,2048,31,64,2112,17,66,2176,171,136,
+    2240,209,420,2304,253,216,2368,367,444,2432,265,456,2496,181,468,2560,39,80,
+    2624,27,164,2688,127,504,2752,143,172,2816,43,88,2880,29,300,2944,45,92,
+    3008,157,188,3072,47,96,3136,13,28,3200,111,240,3264,443,204,3328,51,104,
+    3392,51,212,3456,451,192,3520,257,220,3584,57,336,3648,313,228,3712,271,232,
+    3776,179,236,3840,331,120,3904,363,244,3968,375,248,4032,127,168,4096,31,64,
+    4160,33,130,4224,43,264,4288,33,134,4352,477,408,4416,35,138,4480,233,280,
+    4544,357,142,4608,337,480,4672,37,146,4736,71,444,4800,71,120,4864,37,152,
+    4928,39,462,4992,127,234,5056,39,158,5120,39,80,5184,31,96,5248,113,902,
+    5312,41,166,5376,251,336,5440,43,170,5504,21,86,5568,43,174,5632,45,176,
+    5696,45,178,5760,161,120,5824,89,182,5888,323,184,5952,47,186,6016,23,94,
+    6080,47,190,6144,263,480};
 
 //typedef enum __bool { false = 0, true = 1, } bool;
 
@@ -136,6 +179,7 @@ long seed = 1234421;
 
 
 
+
 #define M	3	// register length,=tail length
 #define NSTATE	8	// = M^2
 #define L_ALL 3*L_TOTAL	// coded frame length
@@ -159,10 +203,6 @@ long seed = 1234421;
 //                      g1(x)    |
 //								 +---------------------->c2
 //
-typedef unsigned char BYTE;
-typedef int INT;
-typedef unsigned int UINT;
-typedef int BOOL;
 
 // NextOut[bk][current state]
 static const char EnNextOut[2][NSTATE] = // check bit based on current and input bit
@@ -195,7 +235,6 @@ static const char TailBit[NSTATE] = // tail info bits when trellis is terminatin
 
 
 
-UINT m_Inter_table[L_TOTAL];
 
 
 
@@ -236,6 +275,11 @@ void init_Block_interleave_table()
 			m_Inter_table[i*temp+j] = j*temp+i;
 
 	
+}
+void init_my_interleave_table() {
+	INT i;
+	for (i=0;i<L_TOTAL;i++)
+		m_Inter_table[i] = L_TOTAL-1-i;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -303,14 +347,18 @@ void encode(BYTE *msg, BYTE *stream, bool puncture)
 }
 
 
-__global__ void interLeave(double * src, double * des , unsigned int * interLeaveTable ){
+__global__ void interLeave(double * src, double * des , unsigned int * interLeaveTable[3] ){
+//__global__ void interLeave(double * src, double * des , unsigned int * interLeaveTable ){
     const int tid = threadIdx.x;
-    des[tid] = src[interLeaveTable[tid]];
+   //des[tid] = src[interLeaveTable[tid]];
+    des[tid] = src[(interLeaveTable[tid][1]*tid+interLeaveTable[tid][2]*tid*tid)%interLeaveTable[tid][0]];
 }
 
-__global__ void deInterLeave(double * src, double * des , unsigned int * interLeaveTable ){
+__global__ void deInterLeave(double * src, double * des , unsigned int * interLeaveTable[3] ){
+//__global__ void deInterLeave(double * src, double * des , unsigned int * interLeaveTable ){
     const int tid = threadIdx.x;
-    des[interLeaveTable[tid]] = src[tid];
+    //des[interLeaveTable[tid]] = src[tid];
+    des[(interLeaveTable[tid][1]*tid+interLeaveTable[tid][2]*tid*tid)%interLeaveTable[tid][0]] = src[tid];
 }
 
 __global__ void gammaAlpha(double * msg ,double * parity, double * L_a, double (*gamma)[8][8], BYTE (*lastState)[8],char (*lastOut)[8] ){
@@ -552,12 +600,15 @@ __global__ void initializeExtrinsicInformation(double * L_e) {
     
 }
 
-__global__ void exestimateInformationBits(double * L_all, BYTE * msghat, UINT * m_Inter_table) {
+__global__ void exestimateInformationBits(double * L_all, BYTE * msghat, UINT * interLeaveTable[3]) {
+//__global__ void exestimateInformationBits(double * L_all, BYTE * msghat, UINT * m_Inter_table) {
     unsigned int tid = threadIdx.x;
     if(L_all[tid]>0)
-        msghat[m_Inter_table[tid]]=1;
+		msghat[(interLeaveTable[tid][1]*tid+interLeaveTable[tid][2]*tid*tid)%interLeaveTable[tid][0]]=1;
+        //msghat[m_Inter_table[tid]]=1;
     else
-        msghat[m_Inter_table[tid]]=0;
+		msghat[(interLeaveTable[tid][1]*tid+interLeaveTable[tid][2]*tid*tid)%interLeaveTable[tid][0]]=0;
+        //msghat[m_Inter_table[tid]]=0;
 }
 
 void countErrors(BYTE *m, BYTE * mhat, UINT * bitsError, UINT * frameError, UINT iter) {
@@ -597,7 +648,8 @@ int main(int argc, char* argv[])
 	y = new double[L_ALL];
 	mhat = new BYTE[L_TOTAL];
 
-	init_Block_interleave_table();	// block interleave
+	//init_Block_interleave_table();	// block interleave
+	//init_my_interleave_table();
 
 
     
@@ -613,7 +665,8 @@ int main(int argc, char* argv[])
 	BYTE * mhatDevice;
 	double * parity0Device;
 	double * parity1Device;
-	UINT * tableDevice;
+	//UINT * tableDevice;
+	UINT * LTETableDevice[3];
 	double * L_eDevice;
 	double * L_aDevice;
 	double * L_allDevice;
@@ -634,7 +687,8 @@ int main(int argc, char* argv[])
     cudaMalloc((void **)&mhatDevice, L_TOTAL*sizeof(BYTE));
     cudaMalloc((void **)&parity0Device, L_TOTAL*sizeof(double));
     cudaMalloc((void **)&parity1Device, L_TOTAL*sizeof(double));
-    cudaMalloc((void **)&tableDevice, L_TOTAL*sizeof(unsigned int));
+    //cudaMalloc((void **)&tableDevice, L_TOTAL*sizeof(unsigned int));
+    cudaMalloc((void **)&LTETableDevice, L_TOTAL*3*sizeof(unsigned int));
     cudaMalloc((void **)&L_eDevice, L_TOTAL*sizeof(double));
     cudaMalloc((void **)&L_aDevice, L_TOTAL*sizeof(double));
     cudaMalloc((void **)&L_allDevice, L_TOTAL*sizeof(double));
@@ -661,7 +715,8 @@ int main(int argc, char* argv[])
     cudaMemcpy(LastOutDevice,LastOut,sizeof(char)*2*8, cudaMemcpyHostToDevice);
     cudaMemcpy(NextOutDevice,NextOut,sizeof(char)*2*8, cudaMemcpyHostToDevice);
 
-    cudaMemcpy(tableDevice,m_Inter_table,sizeof(unsigned int)*L_TOTAL, cudaMemcpyHostToDevice);
+    //cudaMemcpy(tableDevice,m_Inter_table,sizeof(unsigned int)*L_TOTAL, cudaMemcpyHostToDevice);
+    cudaMemcpy(LTETableDevice,LTE_Inter_table,sizeof(unsigned int)*L_TOTAL*3, cudaMemcpyHostToDevice);
 
 	for (Eb_No_dB= 0.0;Eb_No_dB<1.0;Eb_No_dB+=1){
 
@@ -693,12 +748,14 @@ int main(int argc, char* argv[])
 			cudaMemcpy(yDevice,y,sizeof(double)*L_ALL, cudaMemcpyHostToDevice);
 
 			demultiplex<<<1,L_TOTAL>>>(yDevice, msgDevice, parity0Device, parity1Device); 
-			interLeave<<<1,L_TOTAL>>>(msgDevice, imsgDevice, tableDevice);
+			//interLeave<<<1,L_TOTAL>>>(msgDevice, imsgDevice, tableDevice);
+			interLeave<<<1,L_TOTAL>>>(msgDevice, imsgDevice, LTETableDevice);
 			initializeExtrinsicInformation<<<1,L_TOTAL>>>(L_eDevice);
 
 			for (int iter = 0; iter<MAXITER; iter++) {
 				
-				deInterLeave<<<1,L_TOTAL>>>(L_eDevice, L_aDevice, tableDevice);
+			//	deInterLeave<<<1,L_TOTAL>>>(L_eDevice, L_aDevice, tableDevice);
+				deInterLeave<<<1,L_TOTAL>>>(L_eDevice, L_aDevice, LTETableDevice);
 
 				gammaAlpha<<<1,L_TOTAL>>>(msgDevice , parity0Device,  L_aDevice,  gammaAlphaDevice,LastStateDevice, LastOutDevice);
 				gammaBeta<<<1,L_TOTAL>>>(msgDevice , parity0Device,  L_aDevice,  gammaBetaDevice, NextStateDevice, NextOutDevice);
@@ -728,7 +785,8 @@ int main(int argc, char* argv[])
 				//cudaMemcpy(L_aHost1, L_aDevice, sizeof(double)*L_TOTAL, cudaMemcpyDeviceToHost);
 				//}
 
-				interLeave<<<1, L_TOTAL>>>(L_eDevice, L_aDevice, tableDevice);
+				//interLeave<<<1, L_TOTAL>>>(L_eDevice, L_aDevice, tableDevice);
+				interLeave<<<1, L_TOTAL>>>(L_eDevice, L_aDevice, LTETableDevice);
 
 				gammaAlpha<<<1,L_TOTAL>>>(imsgDevice , parity1Device,  L_aDevice,  gammaAlphaDevice, LastStateDevice, LastOutDevice);
 				gammaBeta<<<1,L_TOTAL>>>(imsgDevice , parity1Device,  L_aDevice,  gammaBetaDevice, NextStateDevice, NextOutDevice);
@@ -749,7 +807,8 @@ int main(int argc, char* argv[])
 
 				extrinsicInformation<<<1, L_TOTAL>>>(L_allDevice, imsgDevice, L_aDevice, L_eDevice);
 
-				exestimateInformationBits<<<1,L_TOTAL>>>(L_allDevice, mhatDevice, tableDevice); 
+				exestimateInformationBits<<<1,L_TOTAL>>>(L_allDevice, mhatDevice, LTETableDevice); 
+				//exestimateInformationBits<<<1,L_TOTAL>>>(L_allDevice, mhatDevice, tableDevice); 
 
 				cudaMemcpy(mhat, mhatDevice, sizeof(BYTE)*L_TOTAL, cudaMemcpyDeviceToHost);
 				countErrors(m, mhat, bits_err, frame_err, iter);
@@ -803,7 +862,8 @@ int main(int argc, char* argv[])
 	cudaFree(mhatDevice);
 	cudaFree(parity0Device);
 	cudaFree(parity1Device);
-	cudaFree(tableDevice);
+//	cudaFree(tableDevice);
+	cudaFree(LTETableDevice);
 	cudaFree(L_eDevice);
 	cudaFree(L_aDevice);
 	cudaFree(L_allDevice);
