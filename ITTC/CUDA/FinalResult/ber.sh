22dde7298834f1bb44d3f7191f8052e926f329a4
liuhatry <hatryliu@gmail.com>
2013-07-22 21:10:07 +0800

    Final Ber

--- ITTC/CUDA/FinalResult/ber.sh
+++ ITTC/CUDA/FinalResult/ber.sh
@@ -3,11 +3,12 @@
 max=25
 for i in *Max*
 do
-    block_num=$(cut -d '-' -f1 $i)
-    echo $block_num
+    block_num=$(echo $i)
+    Block=${block_num%%_*txt}
+    BlockNum=$((${Block}*4))
     for (( iter=6; iter<=$max; iter=iter+1 ))
     do
-        echo hello
-        #cat $i | grep 'Ber' | cut -d '=' -f2 | awk 'NR%25 == "'$iter'"{print $0}' > Iters${iter}_${i}
+        cat $i | grep 'Ber' | cut -d '=' -f2 | awk 'NR%25 == "'$iter'"{print $0}' > Iters${iter}_${BlockNum}
     done
 done
+
